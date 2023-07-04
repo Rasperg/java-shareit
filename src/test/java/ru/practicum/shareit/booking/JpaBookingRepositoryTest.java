e23411ba4cf009bb39f89d8cad43081a97dcef50
Denis Golev <110325389+Rasperg@users.noreply.github.com>
2023-07-04 23:12:41 +0500

    add-item-requests

--- src/test/java/ru/practicum/shareit/booking/JpaBookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/JpaBookingRepositoryTest.java
@@ -93,4 +93,132 @@ public class JpaBookingRepositoryTest {
         assertEquals(booker, actualBooking.getBooker());
         assertEquals(BookingStatus.WAITING, actualBooking.getStatus());
     }
+
+    @Test
+    void findByItemIdIn() {
+        List<Long> itemIds = List.of(item.getId());
+        List<Booking> actualBookings = bookingRepository.findByItemIdIn(itemIds);
+
+        assertFalse(actualBookings.isEmpty());
+        assertEquals(1, actualBookings.size());
+
+        Booking actualBooking = actualBookings.get(0);
+        assertNotNull(actualBooking.getStart());
+        assertNotNull(actualBooking.getEnd());
+        assertEquals(item, actualBooking.getItem());
+        assertEquals(booker, actualBooking.getBooker());
+        assertEquals(BookingStatus.WAITING, actualBooking.getStatus());
+    }
+
+    @Test
+    void findByBookerAndStatus() {
+        List<Booking> actualBookings = bookingRepository.findByBookerAndStatus(booker, BookingStatus.WAITING);
+
+        assertFalse(actualBookings.isEmpty());
+        assertEquals(1, actualBookings.size());
+
+        Booking actualBooking = actualBookings.get(0);
+        assertNotNull(actualBooking.getStart());
+        assertNotNull(actualBooking.getEnd());
+        assertEquals(item, actualBooking.getItem());
+        assertEquals(booker, actualBooking.getBooker());
+        assertEquals(BookingStatus.WAITING, actualBooking.getStatus());
+    }
+
+    @Test
+    void findByBookerAndEndIsBefore() {
+        LocalDateTime endDateTime = LocalDateTime.now().minusHours(3);
+        List<Booking> actualBookings = bookingRepository.findByBookerAndEndIsBefore(booker, endDateTime);
+
+        assertTrue(actualBookings.isEmpty());
+    }
+
+    @Test
+    void findByBookerAndStartIsAfter() {
+        LocalDateTime startDateTime = LocalDateTime.now().minusHours(2);
+        List<Booking> actualBookings = bookingRepository.findByBookerAndStartIsAfter(booker, startDateTime);
+
+        assertFalse(actualBookings.isEmpty());
+        assertEquals(1, actualBookings.size());
+
+        Booking actualBooking = actualBookings.get(0);
+        assertNotNull(actualBooking.getStart());
+        assertNotNull(actualBooking.getEnd());
+        assertEquals(item, actualBooking.getItem());
+        assertEquals(booker, actualBooking.getBooker());
+        assertEquals(BookingStatus.WAITING, actualBooking.getStatus());
+    }
+
+    @Test
+    void findByBookerAndStartIsBeforeAndEndIsAfter() {
+        LocalDateTime startDateTime = LocalDateTime.now().plusHours(1);
+        LocalDateTime endDateTime = LocalDateTime.now().minusHours(3);
+        List<Booking> actualBookings = bookingRepository.findByBookerAndStartIsBeforeAndEndIsAfter(booker, startDateTime, endDateTime);
+
+        assertFalse(actualBookings.isEmpty());
+        assertEquals(1, actualBookings.size());
+
+        Booking actualBooking = actualBookings.get(0);
+        assertNotNull(actualBooking.getStart());
+        assertNotNull(actualBooking.getEnd());
+        assertEquals(item, actualBooking.getItem());
+        assertEquals(booker, actualBooking.getBooker());
+        assertEquals(BookingStatus.WAITING, actualBooking.getStatus());
+    }
+
+    @Test
+    void findByItem_OwnerAndStatus() {
+        List<Booking> actualBookings = bookingRepository.findByItem_OwnerAndStatus(owner, BookingStatus.WAITING);
+
+        assertFalse(actualBookings.isEmpty());
+        assertEquals(1, actualBookings.size());
+
+        Booking actualBooking = actualBookings.get(0);
+        assertNotNull(actualBooking.getStart());
+        assertNotNull(actualBooking.getEnd());
+        assertEquals(item, actualBooking.getItem());
+        assertEquals(booker, actualBooking.getBooker());
+        assertEquals(BookingStatus.WAITING, actualBooking.getStatus());
+    }
+
+    @Test
+    void findByItem_OwnerAndEndIsBefore() {
+        LocalDateTime endDateTime = LocalDateTime.now().minusHours(3);
+        List<Booking> actualBookings = bookingRepository.findByItem_OwnerAndEndIsBefore(owner, endDateTime);
+
+        assertTrue(actualBookings.isEmpty());
+    }
+
+    @Test
+    void findByItem_OwnerAndStartIsAfter() {
+        LocalDateTime startDateTime = LocalDateTime.now().minusHours(2);
+        List<Booking> actualBookings = bookingRepository.findByItem_OwnerAndStartIsAfter(owner, startDateTime);
+
+        assertFalse(actualBookings.isEmpty());
+        assertEquals(1, actualBookings.size());
+
+        Booking actualBooking = actualBookings.get(0);
+        assertNotNull(actualBooking.getStart());
+        assertNotNull(actualBooking.getEnd());
+        assertEquals(item, actualBooking.getItem());
+        assertEquals(booker, actualBooking.getBooker());
+        assertEquals(BookingStatus.WAITING, actualBooking.getStatus());
+    }
+
+    @Test
+    void findByItem_OwnerAndStartIsBeforeAndEndIsAfter() {
+        LocalDateTime startDateTime = LocalDateTime.now().plusHours(1);
+        LocalDateTime endDateTime = LocalDateTime.now().minusHours(3);
+        List<Booking> actualBookings = bookingRepository.findByItem_OwnerAndStartIsBeforeAndEndIsAfter(owner, startDateTime, endDateTime);
+
+        assertFalse(actualBookings.isEmpty());
+        assertEquals(1, actualBookings.size());
+
+        Booking actualBooking = actualBookings.get(0);
+        assertNotNull(actualBooking.getStart());
+        assertNotNull(actualBooking.getEnd());
+        assertEquals(item, actualBooking.getItem());
+        assertEquals(booker, actualBooking.getBooker());
+        assertEquals(BookingStatus.WAITING, actualBooking.getStatus());
+    }
 }
